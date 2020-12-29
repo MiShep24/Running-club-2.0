6e11ce370fd2064251cebf87f6c65dabe3cfb33c
MiShep24 <55436440+MiShep24@users.noreply.github.com>
2020-12-29 21:09:16 +0300

    Updated settings

--- runningclub/settings.py
+++ runningclub/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from . import seckey
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = seckey.get_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
